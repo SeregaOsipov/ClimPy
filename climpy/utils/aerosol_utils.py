Accuracy (negative fluxes) with pyDISORT were fixed by switching to double precision via autodoubling. The RF of Tonga H2O is complete.
Some fixes for the coarse dust RF were implemented including the phase function calculation and normalization.

climpy/utils/aerosol_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+
 from climpy.utils import mie_utils as mie
 from climpy.utils.diag_decorators import normalize_size_distribution_by_area
 
@@ -39,7 +40,7 @@ def get_Kok_dust_emitted_size_distribution(moment='dN'):
     print('Kok dVdlogd [0.2-20] area is {}'.format(np.trapz(dVdlogd[ind], logdd[ind])))
 
     vo = dNdlogd_vo
-    if moment is 'dV':
+    if moment == 'dV':
         vo = dVdlogd_vo
     return vo
 
@@ -79,3 +80,5 @@ def derive_aerosols_optical_properties(ri_vo, dA_vo):
     # column_od = np.sum(column_od_by_modes, axis=1)  # sum up modes
 
     return column_od
+
+

climpy/utils/mie_utils.py
@@ -1,10 +1,11 @@
 import miepython as mp
 import numpy as np
-
+import xarray as xr
+import time
 __author__ = 'Sergey Osipov <Serega.Osipov@gmail.com>'
 
 
-def get_mie_efficiencies(ri, r_data, wavelength):
+def get_mie_efficiencies(ri, r_data, wavelength, phase_function_angles_in_radians=None):
     """
     ri = ri_vo['ri'][ind]
     r_data = dA_vo['radii']
@@ -13,30 +14,100 @@ def get_mie_efficiencies(ri, r_data, wavelength):
     :param ri:
     :param r_data: units should be the same as wavelength
     :param wavelength: units should be the same as radius
-    :return:
+    :return:  mie efficiecnies units are microns^2, assuming the input in microns
     """
+
     # mp.mie can take x vector as input
     # these are the ext and sca efficiencies, compare to cross sections (qext * A)
     qext = np.empty((len(wavelength), len(r_data)))
     qsca = np.empty((len(wavelength), len(r_data)))
+    g = np.empty(qext.shape)
+    qasm = np.empty(qext.shape)
+
+    if phase_function_angles_in_radians is None:
+        phase_function_angles_in_radians = np.linspace(0, np.pi, 180)  # angles to evaluate the phase function
+    phase_function = np.empty(qext.shape + phase_function_angles_in_radians.shape)  # wl, r, angle
 
     for wl_index in range(len(wavelength)):
+        print('Wavelength {} out of {}'.format(wl_index, len(wavelength)))
+        # t_s = time.time()
         for r_index in range(len(r_data)):
             m = ri[wl_index]
             # miepython sign convention: imaginary part has to be negative; complex RI = real -j * imaginary
             m = m.real - 1j * np.abs(m.imag)
             x = 2 * np.pi * r_data[r_index] / wavelength[wl_index]
             # efficiencies are without units, area will be in um^2
-            qext[wl_index, r_index], qsca[wl_index, r_index], qback, g = mp.mie(m,x)
+            qext[wl_index, r_index], qsca[wl_index, r_index], qback, g[wl_index, r_index] = mp.mie(m,x)
 
+            # compute the phase function
+            phase_function[wl_index, r_index, :] = mp.i_unpolarized(m, x, np.cos(phase_function_angles_in_radians))  # this is the phase function normalized to ssa
+        # t_e = time.time()
+        # print(t_e-t_s)
     # dummy checks
     if np.any(qext < 0):
         raise Exception('Mie solution can not have negative extinction values, check RI')
 
-    mie_vo = {}
-    mie_vo['qext'] = qext
-    mie_vo['qsca'] = qsca
-    mie_vo['wavelength'] = wavelength
-    mie_vo['r_data'] = r_data
-    return mie_vo
+    mie_ds = xr.Dataset(
+        data_vars=dict(
+            qext=(['wavenumber', 'radius'], qext),
+            qsca=(['wavenumber', 'radius'], qsca),
+            g=(["wavenumber", 'radius'], g),
+            phase_function=(["wavenumber", 'radius', "angle"], phase_function),
+        ),
+        coords=dict(
+            radius=(['radius', ], r_data),
+            wavelength=(['wavenumber', ], wavelength),
+            wavenumber=(['wavenumber', ], 10**4/wavelength),
+            angle=(['angle', ], phase_function_angles_in_radians),
+        ),
+        attrs=dict(description="Optical properties for a given size distribution"),
+    )
+
+    return mie_ds
+
+
+def integrate_mie_over_aerosol_size_distribution(mie_ds, sd_profile_ds):
+    '''
+    Assumptions about units:
+    The input (radius, wavelength) in microns
+    Mie efficiencies are dimensionless and cross-sections are microns^2
+
+    dNdlnr is per profile z length. I.e., to get the column SD, it needs to be integrated in z.
+    dNdlnr units are: number / um^2 / m
+
+    :param mie_ds:
+    :param sd_profile_ds:
+    :return:
+    '''
+
+
+    dNdlnr = sd_profile_ds.dNdlogD  # number / um^2 / m  # (z is in meter
+    A = np.pi * mie_ds.radius ** 2  # um^2
+
+    def integrate_in_log_r(ds):
+        temp_ds = ds.copy(deep=True)
+        temp_ds['radius'] = np.log(temp_ds.radius)
+        result = temp_ds.integrate(coord='radius')
+        return result
+
+    # Have to integrate in log(r), not just r
+    ext_coefficient = integrate_in_log_r(dNdlnr * A * mie_ds.qext)
+    sca_coefficient = integrate_in_log_r(dNdlnr * A * mie_ds.qsca)
+    asy_coefficient = integrate_in_log_r(dNdlnr * A * mie_ds.g * mie_ds.qsca)  # qasm = asymmetryParameter. * A. * qsca;
+    phase_function = integrate_in_log_r(dNdlnr * A * mie_ds.phase_function * mie_ds.qsca) / sca_coefficient
+
+    # Have to integrate in log(r), not just r
+    # ext_coefficient = np.trapz(dNdlnr * A * mie_ds.qext, np.log(mie_ds.radius), axis=0)
+    # sca_coefficient = np.trapz(dNdlnr * A * mie_ds.qsca, np.log(mie_ds.radius), axis=0)
+    # asy_coefficient = np.trapz(dNdlnr * A * mie_ds.g * mie_ds.qsca, np.log(mie_ds.radius), axis=0)
+    # phase_function = np.trapz(dNdlnr * A * mie_ds.phase_function * mie_ds.qsca, np.log(mie_ds.radius), axis=0) / sca_coefficient
+
+    ssa = sca_coefficient / ext_coefficient
+    g = asy_coefficient / sca_coefficient
+
+    op_ds = xr.merge([ext_coefficient.rename('ext'), ssa.rename('ssa'), g.rename('g'), phase_function.rename('phase_function')])
+
+    return op_ds
+
+
 

climpy/utils/optics_utils.py
@@ -18,7 +18,7 @@ function rayleighOpticalPropertiesVO = getRayleighOpticalProperties(odData, wave
 '''
 
 
-def derive_rayleigh_phase_function(cos_theta):
+def derive_rayleigh_phase_function(angle_in_radians):
     '''
     The Rayleigh scattering phase function moments:
     # PMOM = single(zeros([NMOM+1, NLYR]));
@@ -27,13 +27,31 @@ def derive_rayleigh_phase_function(cos_theta):
     :return:
     '''
 
-    p = 3 / 4 * (1 + cos_theta**2)
+    pf = 3 / 4 * (1 + np.cos(angle_in_radians) ** 2)
+    pf = normalize_phase_function(pf, angle_in_radians)
 
-    return p
+    return pf
+
+def normalize_phase_function(pf, angle_in_radians):
+    # TODO: redo using Xarray
+    # normalize PF it to 1.  # https://miepython.readthedocs.io/en/latest/03a_normalization.html
+    mu = np.cos(angle_in_radians)
+    total = 2 * np.pi * np.trapz(pf, mu)
+    if mu[1] < mu[0]:  # 0 to pi angle, 1 to -1 cos(angle)
+        total *= -1  # Invert the sign due to reversed direction of integration.
+
+    # TODO: I'm not sure what the normalization for DISORT should be (1, ssa, 2pi, etc.)
+    total /= 2  # this is how norm was defined in the previous matlab code
+    pf /= total  # normalize to 1
+    return pf
 
 
 def derive_rayleigh_optical_properties(op_ds_w_rayleigh, op_ds_wo_rayleigh):
     '''
+
+    TODO: Would be better parametrize Rayleigh
+    E.g., Rayleigh scattering is following the Nicolet 1984 https://doi.org/10.1016/0032-0633(84)90089-8
+    https://doi.org/10.1175/1520-0426(1999)016%3C1854:ORODC%3E2.0.CO;2
 
     :param op_ds_w_rayleigh: is the LBLRTM with Rayleigh included
     :param op_ds_wo_rayleigh: the same, without Rayleigh scattering
@@ -41,16 +59,15 @@ def derive_rayleigh_optical_properties(op_ds_w_rayleigh, op_ds_wo_rayleigh):
     '''
     rayleigh_od_da = op_ds_w_rayleigh.od - op_ds_wo_rayleigh.od
     # TODO: maybe it is better do make a deep copy and edit from there
-    pf_cos_angle = op_ds_w_rayleigh.angle.data  # op_ds store the cos of angle as an angle
-    rayleigh_op_ds = get_rayleigh_optical_properties(rayleigh_od_da, pf_cos_angle)
+    rayleigh_op_ds = get_rayleigh_optical_properties(rayleigh_od_da, op_ds_w_rayleigh.angle.data)  # op_ds store the angle in radiance
 
     return rayleigh_op_ds
 
 
-def get_rayleigh_optical_properties(rayleigh_od_da, pf_cos_angle):
+def get_rayleigh_optical_properties(rayleigh_od_da, angle_in_radians):
     ssa = np.ones(rayleigh_od_da.shape)
     g = np.zeros(rayleigh_od_da.shape)  # alternative is to derive from PF
-    phase_function = derive_rayleigh_phase_function(pf_cos_angle)
+    phase_function = derive_rayleigh_phase_function(angle_in_radians)
     phase_function = np.tile(phase_function, rayleigh_od_da.shape[0:2] + (1,))
 
     op_ds_rayleigh = xr.Dataset(
@@ -64,7 +81,7 @@ def get_rayleigh_optical_properties(rayleigh_od_da, pf_cos_angle):
             level=(['level', ], rayleigh_od_da.level.data),
             wavenumber=(['wavenumber', ], rayleigh_od_da.wavenumber.data),
             wavelength=(['wavenumber', ], rayleigh_od_da.wavelength.data),
-            angle=(['angle', ], pf_cos_angle),  # op_ds actually stores cos of angles as "angle" variable
+            angle=(['angle', ], angle_in_radians),  # op_ds stores the angle in radiance
         ),
         attrs=dict(description="Rayleigh optical properties"),
     )
@@ -83,17 +100,20 @@ def mix_optical_properties(ops, externally=True):
     bulk_ds = xr.concat(ops, dim='species')
     # aod is just a sum
     od_ds = bulk_ds.od.sum(dim='species')
+
     # ssa is weighted by extinction
     weights = bulk_ds.od/bulk_ds.od.sum(dim='species')
     weights.name = 'weight'
+    if (bulk_ds.od.sum(dim='species')==0).any():  # then I will get NaN due to division by 0
+        weights = weights.fillna(0)
     ssa_ds = bulk_ds.ssa.weighted(weights).sum(dim='species')
+
     # g and phase function are weighted by extinction * ssa
     od_times_ssa_ds = bulk_ds.od * bulk_ds.ssa
     weights = od_times_ssa_ds / od_times_ssa_ds.sum(dim='species')
     weights.name = 'weight'
-    if weights.isnull().any():
+    if (od_times_ssa_ds.sum(dim='species')==0).any():  # then I will get NaNs due to division by 0
         weights = weights.fillna(0)
-        print('mix_optical_properties: replacing missing weights with 0. This could happen when SSA is exactly zero. Otherwise, search for an error.')
     g_ds = bulk_ds.g.weighted(weights).sum(dim='species')
     pf_ds = bulk_ds.phase_function.weighted(weights).sum(dim='species')
 